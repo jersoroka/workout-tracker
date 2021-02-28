5f7cd96593d872a24d37484c15988890bb50d45b
jerso <jersoroka@gmail.com>
2021-02-27 16:57:24 -0800

    Starting writing JsonWriterTest

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,14 +1,5 @@
 package persistence;
 
-import model.WorkoutSet;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class JsonWriterTest extends JsonTest{
 
 }
